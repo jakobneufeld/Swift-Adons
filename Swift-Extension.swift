0944ef50b1df32205df31b0c0024b2c507d8cdd9
Niko Neufeld <niko.neufeld@gmail.com>
2019-07-31 16:21:10 +0200

    I updated it so that the compiler is happy

--- Swift-Extension.swift
+++ Swift-Extension.swift
@@ -3,38 +3,38 @@
 //  Swift-Extension
 //
 //  Created by Niko Neufeld on 7/31/19.
-//
-
-import Foundation
 precedencegroup plusPlusAndMinusMinus {
-    higherThan: BitwiseShiftPrecedence
+    higherThan: PowerOperatorPrecedence
     assignment: false
     associativity: none
-}
-postfix operator ++: plusPlusAndMinusMinus
-public postfix func ++(rhs: Int) {
+ }
+ postfix operator ++
+ public postfix func ++(rhs: Int) -> Int {
     return rhs + 1
-}
-postfix operator --: plusPlusAndMinusMinus
-public postfix func --(rhs: Int) {
+ }
+ postfix operator --
+ public postfix func --(rhs: Int) -> Int{
     return rhs - 1
-}
-
-precedencegroup PowerOperatorPrecedence {
-higherThan: MultiplicationPrecedence
-lowerThan: plusPlusAndMinusMinus
-associativity: none
-assignment: false
-}
-
-infix operator **: PowerOperatorPrecedence
-public infix func **(lhs: Int, rhs: Int) {
+ }
+ 
+ precedencegroup PowerOperatorPrecedence {
+    higherThan: MultiplicationPrecedence
+    lowerThan: BitwiseShiftPrecedence
+    associativity: none
+    assignment: false
+ }
+ 
+ infix operator **: PowerOperatorPrecedence
+ public func **(lhs: Int, rhs: Int) -> Int  {
     if rhs == 0 {
         return 1    
     }
-    r = lhs
+    var r = lhs
     for _ in 1..<rhs {
         r = r * lhs
     }
     return r
-}
+ }
+ 
+
+
