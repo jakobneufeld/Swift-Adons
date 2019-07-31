840ff9ca4fedba505599068f06af25c260ec763e
Niko Neufeld <niko.neufeld@gmail.com>
2019-07-31 12:02:26 +0200

    Update Swift-Extension.swift
    Added the power operator ** (as in FORTRAN) For Ints only to start with. Should use generic types

--- Swift-Extension.swift
+++ Swift-Extension.swift
@@ -14,3 +14,15 @@ postfix operator --
 public postfix func --(rhs: Int) {
     return rhs - 1
 }
+
+infix operator **
+public infix func **(lhs: Int, rhs: Int) {
+    if rhs == 0 {
+        return 1    
+    }
+    r = lhs
+    for _ in 1..<rhs {
+        r = r * lhs
+    }
+    return r
+}
